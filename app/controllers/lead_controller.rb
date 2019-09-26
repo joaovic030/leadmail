ffe34a54e929d6853bd53ffb8a2da6320b92618b
joaorocha <joao.victor@webtechbr.com>
2019-09-26 16:15:32 -0400

    Recebendo arquivo de email com gem Mail funcionando

--- app/controllers/lead_controller.rb
+++ app/controllers/lead_controller.rb
@@ -7,32 +7,42 @@ class LeadController < ApplicationController
   end
 
   def receive_file
-    @page = Nokogiri::HTML(open('../leadmail/email_received.eml'))
-    # p @page
-    puts " HEEEEEREEEEEEEE >>>>>>>>>>>>>>>>>>>>>>>  #{@page.css("font").map {|b| b.text}} <<<<<<<<<<<<<<<<<"
-    @all_elements = @page.css("font").map {|b| b.text} unless @page.nil?
-    @link = @page.css("font").css("a").text
-    # puts "------------------------------------ #{@link}"
-    # @page_link = Nokogiri::HTML(open("#{@link}")) unless @link.nil?
+    # fields = {}
 
-    begin
-      page = RestClient::Request.execute(method: :get, url: "https://www.webmotors.com.br/comprar/volkswagen/golf/1-0-200-tsi-total-flex-comfortline-tiptronic/4-portas/2017-2018/29906350?pos=a29906350g:&np=1", proxy: nil)
-    rescue RestClient::Unauthorized, RestClient::Forbidden => err
-      puts 'Access denied'
-      flash[:danger] = "Access denied"
-      return err.response
-    rescue RestClient::ImATeapot => err
-      puts 'The server is a teapot! # RFC 2324'
-      return err.response
-    else
-      p 'Worked!'
-      page = Nokogiri::HTML(page.body)
-      p page.css("li")
-      return page
-    end
+    LeadHelper.receive_email
+    @page = Nokogiri::HTML(open('/home/webtech/RubymineProjects/leadmail/emails/email'))
+    # # p @page
+    # puts " HEEEEEREEEEEEEE >>>>>>>>>>>>>>>>>>>>>>>  #{@page.css("font").map {|b| b.text}} <<<<<<<<<<<<<<<<<"
+    @all_elements = @page.css("font b").map {|b| b.text} unless @page.nil?
+    p @all_elements
+    # fields['nome'] = @all_elements.to_s.downcase.include? "nome"
+    # fields['telefone'] = @all_elements.to_s.downcase.include? "telefone"
+    # fields['mensagem'] = @all_elements.to_s.downcase.include? "mensagem"
+    # fields['veiculo'] = @all_elements.to_s.downcase.include? "veiculo"
+    # fields['valor'] = @all_elements.to_s.downcase.include? "valor"
+    # puts "???????? #{@all_elements =~ /Nome/}"
+    # puts ">>>>>># #{fields}"
+    # @link = @page.css("font").css("a").text
+    # # puts "------------------------------------ #{@link}"
+    # # @page_link = Nokogiri::HTML(open("#{@link}")) unless @link.nil?
+    #
+    # begin
+    #   @page = RestClient::Request.execute(method: :get, url: "https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal", proxy: nil)
+    # rescue RestClient::Unauthorized, RestClient::Forbidden => err
+    #   puts 'Access denied'
+    #   flash[:danger] = "Access denied"
+    #   return err.response
+    # rescue RestClient::ImATeapot => err
+    #   puts 'The server is a teapot! # RFC 2324'
+    #   return err.response
+    # else
+    #   p 'Worked!'
+    #   # p @page.body
+    #   # @page = Nokogiri::HTML(@page.body)
+    #   # @anchor = @page.css("a").map { |a| a['href']}
+    # end
 
     # puts Mail.all
-    LeadHelper.receive_email
 
     #proxima pagina detalhes do carro
     # css("li.VehicleDetails__list__item")
--- app/helpers/lead_helper.rb
+++ app/helpers/lead_helper.rb
@@ -5,39 +5,54 @@ module LeadHelper
   require 'fiddle'
 
   def self.receive_email
-    # Mail.defaults do
-    #   retriever_method :pop3, :address    => "pop.gmail.com",
-    #                    :port       => 995,
-    #                    :user_name  => 'leadmail030@gmail.com',
-    #                    :password   => 'LEADMAIL030',
-    #                    :enable_ssl => true
-    # end
-    # email_first = Mail.first
-    # p "OVER HEEEEERREEEE vvv"
-    # puts email_first.parts.first.decoded
-
-
-
-    Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
-    Net::POP3.start('pop.gmail.com', 995, 'leadmail030@gmail.com', 'LEADMAIL030') do |pop|
-
-      if pop.mails.empty?
-        puts 'No mails.'
-      else
-        puts "Mails present"
-        pop.each_mail do |mail|
-          p mail
-          p mail.header(''.dup)
-          contentbody = mail.pop(''.dup)
-          newContent = contentbody.force_encoding('UTF-8')
-          @content = Base64.decode64("#{newContent}")
-          # message = Base64.decode64(content.to_s)
-          p ">>>>>>>>>>>>>>>>>>>>>>>>>>>O CONTEUDO ESTA CONVERTIDO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-          p @content
-
-        end
-      end
-      return @content
+    email_dir = '/home/webtech/RubymineProjects/leadmail/emails/'
+    filename = "email"
+    Mail.defaults do
+      retriever_method :pop3, :address    => "pop.gmail.com",
+                       :port       => 995,
+                       :user_name  => 'leadmail030@gmail.com',
+                       :password   => 'LEADMAIL030',
+                       :enable_ssl => true
     end
+    unless Mail.first.blank?
+      email_first = Mail.first.parts
+      @email_decoded = email_first.parts.second.decoded
+      begin
+        File.open(email_dir + filename, "w+b", 0644) {|f| f.write @email_decoded}
+        p "Succefull writed"
+      rescue => e
+        puts "Unable to save data for #{filename} because #{e.message}"
+      end
+    else
+      p 'No mails!'
+    end
+    p "OVER HEEEEERREEEE vvv"
+
   end
+
+
+
+
+    # Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
+    # Net::POP3.start('pop.gmail.com', 995, 'leadmail030@gmail.com', 'LEADMAIL030') do |pop|
+    #
+    #   if pop.mails.empty?
+    #     puts 'No mails.'
+    #   else
+    #     puts "Mails present"
+    #     pop.each_mail do |mail|
+    #       p mail
+    #       p mail.header(''.dup)
+    #       contentbody = mail.pop(''.dup)
+    #       newContent = contentbody.force_encoding('UTF-8')
+    #       @content = Base64.decode64("#{newContent}")
+    #       # message = Base64.decode64(content.to_s)
+    #       p ">>>>>>>>>>>>>>>>>>>>>>>>>>>O CONTEUDO ESTA CONVERTIDO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+    #       p @content
+    #
+    #     end
+    #   end
+    #   return @content
+    # end
+  # end
 end
